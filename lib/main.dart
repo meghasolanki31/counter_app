b83b3b6978c320ecbf6be3d62a1e7e2aca9a1963
megha <meghasolanki31@gmail.com>
2025-06-06 17:28:58 +0530

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:counter_app/font_size_change.dart';
+//if you want run color change dart file then uncomment this line
+// import 'package:counter_app/color_change_dart.dart';
 import 'package:flutter/material.dart';
 
 void main() {
